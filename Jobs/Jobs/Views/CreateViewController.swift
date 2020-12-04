605fe17ee891264b8cacf0be806951a9a02bdfe3
Leticia Malipense <leticia.malipense@gmail.com>
2020-12-03 22:49:13 -0300

    Adicionando telas

---
+++ Jobs/Jobs/Views/CreateViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CreateViewController.swift
+//  Jobs
+//
+//  Created by Leticia Malipense on 03/12/20.
+//
+
+import UIKit
+
+class CreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Jobs/Jobs/Views/ViewController.swift
+++ Jobs/Jobs/Views/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   
+    }
 
 }
 
